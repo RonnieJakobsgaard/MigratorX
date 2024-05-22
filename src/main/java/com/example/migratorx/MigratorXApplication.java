679b0fc597e1ace9b06a9b88ec281818957a4882
Ronnie Jakobsgaard <ronnie192@gmail.com>
2024-05-22 19:40:45 +0200

    Initial MigratorX project

---
+++ src/main/java/com/example/migratorx/MigratorXApplication.java
@@ -0,0 +1,13 @@
+package com.example.migratorx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratorXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratorXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/migratorx/MigratorXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migratorx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigratorXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
